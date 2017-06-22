61034a1c1f126d60c215b5eef7e8564b9b9a9b07
wangchao <magician.of.technique@gmail.com>
2017-06-22 10:56:25 +0800

    [optimize] schedule-host.sh脚本：优化脚本，追加自己的配置。

--- easy-hosts/schedule-host.sh
+++ easy-hosts/schedule-host.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# cd 需要先 cd 到该脚本所在目录 
+# cd 需要先 cd 到该脚本所在目录 eg. cd /xx/xx/xx/easyhosts
 git pull
+cp hosts.txt hosts.copy
+# 追加我们自己的配置
+echo "0.0.0.0 account.jetbrains.com" >> hosts.copy
 echo "password"|sudo -S cp -rf  hosts.txt /etc/hosts
